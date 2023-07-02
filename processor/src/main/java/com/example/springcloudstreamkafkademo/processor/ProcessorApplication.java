19389d0a53112db70ff8fcd3f1d16e91fcaa730f
Bernard Budano <bbudano@gmail.com>
2023-07-02 22:31:50 +0200

    Consume example message in processor service

--- processor/src/main/java/com/example/springcloudstreamkafkademo/processor/ProcessorApplication.java
+++ processor/src/main/java/com/example/springcloudstreamkafkademo/processor/ProcessorApplication.java
@@ -1,13 +1,23 @@
 package com.example.springcloudstreamkafkademo.processor;
 
+import com.example.springcloudstreamkafkademo.integration.ExampleMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class ProcessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessorApplication.class, args);
+	}
+
+	@Bean
+	Consumer<Message<ExampleMessage>> processMessage() {
+		return message -> System.out.println(message.getPayload());
 	}
 
 }

6be7b7e3b7feb230a06191cf5245525380bb0c2f
Bernard Budano <bbudano@gmail.com>
2023-07-03 01:33:52 +0200

    Consume processed message in consumer service

--- consumer/src/main/java/com/example/springcloudstreamkafkademo/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/springcloudstreamkafkademo/consumer/ConsumerApplication.java
@@ -1,13 +1,25 @@
 package com.example.springcloudstreamkafkademo.consumer;
 
+import com.example.springcloudstreamkafkademo.integration.ProcessedMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 
+import java.util.function.Consumer;
+
+@Slf4j
 @SpringBootApplication
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@Bean
+	Consumer<Message<ProcessedMessage>> consume() {
+		return message -> log.info("Consumed message: {}", message.getPayload());
 	}
 
 }
